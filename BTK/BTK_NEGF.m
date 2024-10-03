e5091fb6a95279b4a502001252f56c0da237dcfd
DebasishPanda529 <pandad091@gmail.com>
2024-10-03 22:43:09 +0530

    updated report

--- BTK/BTK_NEGF.m
+++ BTK/BTK_NEGF.m
@@ -52,52 +52,15 @@ end
 
 % % I1_UB(n,:) = real(I1); 
 
-figure(1)
-hold on
-for u=1:length(UB_array)
-   plot(V./Delta2, real(I1_all(u, :))./Delta2, colors(u), 'linewidth', 2);
-   % text(V(end)./Delta2, real(I1_all(u, end))./Delta2, labels{u}, 'FontSize', 20, 'HorizontalAlignment', 'right')
-end
-
-% title('I-V Characteristics','fontSize',30,'interpreter','latex')
-ylabel('Current $(e\Delta_0/\hbar)$','interpreter','latex','fontsize',30)
-xlabel('Bias $(eV/\Delta_0)$','fontSize',30,'interpreter','latex')
-legend({'U = 0', 'U = 0.5t', 'U = t', 'U = 2t'}, 'Fontsize', 20)
-set(gca,'fontSize',30,'linewidth',2,'fontSize',30)
-
-ax = gca;
-ax.XAxis.TickDirection = 'in';
-ax.YAxis.TickDirection = 'in';
-ax.XAxis.MinorTick = 'on';
-ax.YAxis.MinorTick = 'on';
-ax.XAxis.TickLength = [0.01, 0.005];
-ax.YAxis.TickLength = [0.01, 0.005];
-ax.FontSize = 20;
-ax.Box = 'on';
-ax.GridLineStyle = ':';
-grid on
-grid minor
-
-ylim([0 8])
-yticks(0:1:8)
-ax.YAxis.MinorTickValues = 0:0.5:8;
-
-xlim([0 2])
-xticks(0:0.5:2)
-ax.XAxis.MinorTickValues = 0:0.25:2;
-  %%
-
-% % figure(2)
 % figure(1)
 % hold on
-% dV = V(2)-V(1);
 % for u=1:length(UB_array)
-%    plot(V(1:length(V)-1)./Delta2,G1_all(u,:),colors(u),'linewidth', 2);
-%     % text(V(end-1)./Delta2, G1_all(u, end), labels{u}, 'FontSize', 20, 'HorizontalAlignment', 'right')
+%    plot(V./Delta2, real(I1_all(u, :))./Delta2, colors(u), 'linewidth', 2);
+%    % text(V(end)./Delta2, real(I1_all(u, end))./Delta2, labels{u}, 'FontSize', 20, 'HorizontalAlignment', 'right')
 % end
-% 
-% % title('Conductance','fontSize',30,'interpreter','latex')
-% ylabel('G $(e^2/\hbar)$','interpreter','latex','fontsize',30)
+
+% title('I-V Characteristics','fontSize',30,'interpreter','latex')
+% ylabel('Current $(e\Delta_0/\hbar)$','interpreter','latex','fontsize',30)
 % xlabel('Bias $(eV/\Delta_0)$','fontSize',30,'interpreter','latex')
 % legend({'U = 0', 'U = 0.5t', 'U = t', 'U = 2t'}, 'Fontsize', 20)
 % set(gca,'fontSize',30,'linewidth',2,'fontSize',30)
@@ -115,10 +78,47 @@ ax.XAxis.MinorTickValues = 0:0.25:2;
 % grid on
 % grid minor
 % 
-% ylim([0 4.5])
-% yticks(0:0.5:4.5)
-% ax.YAxis.MinorTickValues = 0:0.25:4.5;
+% ylim([0 8])
+% yticks(0:1:8)
+% ax.YAxis.MinorTickValues = 0:0.5:8;
 % 
 % xlim([0 2])
 % xticks(0:0.5:2)
 % ax.XAxis.MinorTickValues = 0:0.25:2;
+  %%
+
+% figure(2)
+figure(1)
+hold on
+dV = V(2)-V(1);
+for u=1:length(UB_array)
+   plot(V(1:length(V)-1)./Delta2,G1_all(u,:),colors(u),'linewidth', 2);
+    % text(V(end-1)./Delta2, G1_all(u, end), labels{u}, 'FontSize', 20, 'HorizontalAlignment', 'right')
+end
+
+% title('Conductance','fontSize',30,'interpreter','latex')
+ylabel('G $(e^2/\hbar)$','interpreter','latex','fontsize',30)
+xlabel('Bias $(eV/\Delta_0)$','fontSize',30,'interpreter','latex')
+legend({'U = 0', 'U = 0.5t', 'U = t', 'U = 2t'}, 'Fontsize', 20)
+set(gca,'fontSize',30,'linewidth',2,'fontSize',30)
+
+ax = gca;
+ax.XAxis.TickDirection = 'in';
+ax.YAxis.TickDirection = 'in';
+ax.XAxis.MinorTick = 'on';
+ax.YAxis.MinorTick = 'on';
+ax.XAxis.TickLength = [0.01, 0.005];
+ax.YAxis.TickLength = [0.01, 0.005];
+ax.FontSize = 20;
+ax.Box = 'on';
+ax.GridLineStyle = ':';
+grid on
+grid minor
+
+ylim([0 4.5])
+yticks(0:0.5:4.5)
+ax.YAxis.MinorTickValues = 0:0.25:4.5;
+
+xlim([0 2])
+xticks(0:0.5:2)
+ax.XAxis.MinorTickValues = 0:0.25:2;
